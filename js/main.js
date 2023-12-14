9f7e1409470d09cfef0951c6575de04c6fdc953b
dragpunnn <98554667+dragpunnn@users.noreply.github.com>
2023-12-14 15:16:58 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
-// Check if panic is already enabled
 if (localStorage.getItem("cloak") == "dragpunnn") {
   document.querySelector("link[rel='icon']").href = "/imgs/Logo.png";
   document.title = "Dragpunnn";
-  console.log("railin");
+  console.log("railin")
 } 
 
 if (localStorage.getItem("cloak") == "classroom") {
   document.querySelector("link[rel='icon']").href = "/imgs/cloaks/classroom.png";
   document.title = "Classroom";
-  console.log("classroom");
+  console.log("classroom")
 } 
 
 if (localStorage.getItem("cloak") == "canvas") {
   document.querySelector("link[rel='icon']").href = "/imgs/cloaks/canvas.png";
   document.title = "Canvas";
-  console.log("canvas");
+  console.log("canvas")
 } 
 
 if (localStorage.getItem("cloak") == "ps") {
   document.querySelector("link[rel='icon']").href = "/imgs/cloaks/powerschool.png";
   document.title = "Grades and Attendance";
-  console.log("powerschool");
+  console.log("powerschool")
 }
 
 if (localStorage.getItem("cloak") == "google") {
   document.querySelector("link[rel='icon']").href = "/imgs/cloaks/google.png";
   document.title = "Google";
-  console.log("google");
+  console.log("google")
 }
 
 if (localStorage.getItem("cloak") == "drive") {
   document.querySelector("link[rel='icon']").href = "/imgs/cloaks/drive.png";
   document.title = "My Drive - Google Drive";
-  console.log("drive");
+  console.log("drive")
 }
     
 if (localStorage.getItem("panic") == "on") {
@@ -40,15 +39,8 @@ if (localStorage.getItem("panic") == "on") {
     if (event.keyCode == 192) {
       window.open("https://www.google.com/", "_self");
     }
-  });
-
-  // Change background color of all elements with the class "button1"
-  var buttons = document.getElementsByClassName("button1");
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].style.backgroundColor = "green";
-  }
-
-  console.log('panic is enabled');
+  })
+  console.log('panic')
 }
 
 if (localStorage.getItem("panic") == "off") {
@@ -70,10 +62,5 @@ window.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem("theme") === "amoled") {
     document.body.setAttribute("class", "amoled");
     console.log('amoled');
-  }
-
-  if (localStorage.getItem("theme") === "ugly") {
-    document.body.setAttribute("class", "ugly");
-    console.log('ugly');
   }
 });
